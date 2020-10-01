b9ae811bccfefdaa5ef4831529f894558c858a3b
Rod-S <rodsanati@gmail.com>
2020-10-01 15:11:12 -0500

    passing isFive test and solution added

--- code.js
+++ code.js
@@ -6,3 +6,7 @@ function helloWorld() {
 function sayHello(input) {
  return (input && typeof input !== 'boolean' && typeof input !== 'number') ? "Hello, " + input + "!" : "Hello, World!";
 }
+
+function isFive(input) {
+    return (input == 5)
+}
--- tests.js
+++ tests.js
@@ -44,3 +44,14 @@ describe('sayHello', function(){
     });
 });
 
+describe('isFive', function(){
+    it('should be a defined function', function() {
+        expect(typeof isFive).toBe('function');
+    });
+    it('should return a boolean', function() {
+       expect(typeof isFive()).toBe("boolean");
+    });
+    it('should return true when passed 5', function() {
+        expect(isFive(5)).toBe(true);
+    });
+});
